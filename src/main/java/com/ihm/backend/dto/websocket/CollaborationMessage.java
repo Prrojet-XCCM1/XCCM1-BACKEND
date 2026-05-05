2db03866c9f121a9763aaef9375c339a554c688a
Delmat237 <azangueleonel9@gmail.com>
2026-05-05 12:39:21 +0100

    SAVe I think oo

--- src/main/java/com/ihm/backend/dto/websocket/CollaborationMessage.java
+++ src/main/java/com/ihm/backend/dto/websocket/CollaborationMessage.java
@@ -11,7 +11,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class CollaborationMessage {
     public enum MessageType {
-        MOVE, LOCK, UNLOCK, CURSOR, ERROR
+        MOVE, LOCK, UNLOCK, CURSOR, BLOCK_UPDATE, ERROR
     }
 
     private MessageType type;
