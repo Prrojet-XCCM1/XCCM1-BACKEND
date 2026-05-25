78b2584eda4ee33f85bfb13739384f95dfb40c40
Delmat237 <azangueleonel9@gmail.com>
2026-05-25 09:07:00 +0100

    feat: add Collections import to CourseController

--- src/main/java/com/ihm/backend/controller/CourseController.java
+++ src/main/java/com/ihm/backend/controller/CourseController.java
@@ -17,6 +17,7 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.UUID;
